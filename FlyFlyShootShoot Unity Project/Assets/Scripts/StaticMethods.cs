27cb3bd07df3b4528fce2eb3c47bcd0c67dc5d4b
MGrin <mr6r1n@gmail.com>
2014-05-15 14:21:29 +0200

    Modified the asteroid material - now we can see them =)

---
+++ FlyFlyShootShoot Unity Project/Assets/Scripts/StaticMethods.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticMethods : MonoBehaviour {
+
+	public static bool Pause = false;
+
+	public static void SetOnPause(){
+		if (!Pause) {
+			Pause = true;
+		}
+	}
+
+	public static void SetOnPlay(){
+		if (Pause) {
+			Pause = false;
+		}
+	}
+}
